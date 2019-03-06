b56b965acbb85f04f470fe4491222ee7899db56b
Reynault Sies <46537102+Reynault@users.noreply.github.com>
2019-03-06 17:51:56 +0100

    Add files via upload
    Serveur : Début de l'implémentation du serveur java

--- Serveur/src/mycalendar/modele/serveur/ConnexionClient.java
+++ Serveur/src/mycalendar/modele/serveur/ConnexionClient.java
@@ -1,4 +1,24 @@
 package mycalendar.modele.serveur;
 
-public class ConnexionClient {
+import java.io.IOException;
+import java.net.Socket;
+
+public class ConnexionClient implements Runnable{
+
+    private Socket socket;
+
+    public ConnexionClient(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try {
+            System.out.println("Un client s'est connecté.");
+            socket.close();
+            System.out.println("On ferme le client.");
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
