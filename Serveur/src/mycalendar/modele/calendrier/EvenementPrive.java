1606c9050ed3d7de06db480b604adc139f04afd5
Bert54 <matt.bert@live.fr>
2019-03-07 13:49:14 +0100

    Add event (not fully implemented)

--- Serveur/src/mycalendar/modele/calendrier/EvenementPrive.java
+++ Serveur/src/mycalendar/modele/calendrier/EvenementPrive.java
@@ -1,4 +1,12 @@
 package mycalendar.modele.calendrier;
 
-public class EvenementPrive {
+import java.util.GregorianCalendar;
+
+public class EvenementPrive extends Evenement {
+
+    public EvenementPrive(int id, int calendrierID, String nom, String description, String image, GregorianCalendar date, String lieu, String auteur) {
+        super(id, calendrierID, nom, description, image, date, lieu, auteur);
+        this.visibilite = false;
+    }
+
 }
--- Serveur/src/mycalendar/modele/calendrier/EvenementPublic.java
+++ Serveur/src/mycalendar/modele/calendrier/EvenementPublic.java
@@ -1,4 +1,12 @@
 package mycalendar.modele.calendrier;
 
-public class EvenementPublic {
+import java.util.GregorianCalendar;
+
+public class EvenementPublic extends Evenement {
+
+    public EvenementPublic(int id, int calendrierID, String nom, String description, String image, GregorianCalendar date, String lieu, String auteur) {
+        super(id, calendrierID, nom, description, image, date, lieu, auteur);
+        this.visibilite = true;
+    }
+
 }
