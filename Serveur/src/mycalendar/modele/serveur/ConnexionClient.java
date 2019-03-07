43ec73dc02b7974adafb516d41d9851d0740fc69
Reynault Sies <46537102+Reynault@users.noreply.github.com>
2019-03-07 13:43:44 +0100

    Add files via upload
    Serveur : Mise à jour de la connexion client

--- Serveur/src/mycalendar/modele/serveur/ConnexionClient.java
+++ Serveur/src/mycalendar/modele/serveur/ConnexionClient.java
@@ -1,6 +1,7 @@
 package mycalendar.modele.serveur;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class ConnexionClient implements Runnable{
@@ -15,7 +16,25 @@ public class ConnexionClient implements Runnable{
     public void run(){
         try {
             System.out.println("Un client s'est connecté.");
+
+            System.out.printf(socket.getInetAddress().toString());
+
+            System.out.println("Récupération de la donnée sous la forme d'une hashmap avec" +
+                    " le parseur json");
+
+            BufferedReader bos = new BufferedReader(
+                    new InputStreamReader(
+                            socket.getInputStream()));
+            String ligne;
+            ligne = bos.readLine();
+
+            System.out.println("Ligne envoyée par le client.");
+
+            System.out.printf("Traite les données avec un switch (En fonction du type de requête, méthode différente" +
+                    " de ApplicationServeur");
+
             socket.close();
+
             System.out.println("On ferme le client.");
         }catch (IOException e){
             System.out.println(e.getMessage());
