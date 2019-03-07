f1e270e626eb780db4609bccc85803b7770a96d6
Reynault Sies <46537102+Reynault@users.noreply.github.com>
2019-03-07 16:51:07 +0100

    Add files via upload
    Serveur : Modification de l'application Serveur

--- Serveur/src/mycalendar/modele/serveur/ApplicationServeur.java
+++ Serveur/src/mycalendar/modele/serveur/ApplicationServeur.java
@@ -1,9 +1,15 @@
 package mycalendar.modele.serveur;
 
+import mycalendar.modele.bdd.GestionnaireBDD;
+import mycalendar.modele.utilisateur.Utilisateur;
+
+import javax.rmi.CORBA.Util;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -47,5 +53,63 @@ public class ApplicationServeur implements Observer {
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    /**
+     * Lorsqu'un utilisateur souhaite se connecter, cette fonction teste s'il est présent dans la base de données.
+     * S'il est déjà présent alors la connexion sera possible.
+     * @param email L'email de l'utilisateur
+     * @param mdp Le mot de passe de l'utilisateur
+     * @return 0 si la connexion a échoué, 1 si c'est réussi
+     * @throws SQLException
+     */
+    public HashMap<String, String> authentification(String email, String mdp) throws SQLException {
+        HashMap<String, String> res = new HashMap<String, String>();
+        res.put("Request","SignIn");
+        // Vérification de la connexion
+        if(Utilisateur.verifierConnexion(email, mdp)){
+            // Récupération des calendriers de l'utilisateur
+
+        }else{
+            // Utilisateur non trouvé
+            res.put("Result","User not found");
+        }
+        return res;
+    }
+
+    /**
+     * Méthode inscription qui correspond à une inscription d'un utilisateur dans la base de données
+     * @param email email de l'utilisateur
+     * @param mdp mot de passe chiffré
+     * @param prenom prénom de l'utilisateur
+     * @param nom nom de l'utilisateur
+     * @return une hashmap qui contient les informations à envoyer au client
+     * @throws SQLException
+     */
+    public HashMap<String, String> inscription(String email, String mdp, String prenom, String nom) throws SQLException{
+        HashMap<String, String> res = new HashMap<String, String>();
+        res.put("Request","SignIn");
+        // Vérification de l'inscription
+        switch(Utilisateur.verifierInscription(email, mdp, prenom, nom)){
+            case 1:
+            {
+                // Inscription réussie
+                res.put("Result","Success");
+                break;
+            }
+            case 0:
+            {
+                // Utilisateur déjà existant
+                res.put("Result","Username already exist");
+                break;
+            }
+            case 2:
+            {
+                // Cas dans lequel une des données est trop longue
+                res.put("Result","Données trop longues");
+                break;
+            }
+        }
+        return res;
     }
 }
