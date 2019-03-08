badf64e34a5e680a6dd3ffe3ac22323075df0d6c
Bert54 <matt.bert@live.fr>
2019-03-08 21:08:37 +0100

    Server side - delete event (with fixed getCalendarID())

--- Serveur/src/mycalendar/modele/clientTest/ClientTest.java
+++ Serveur/src/mycalendar/modele/clientTest/ClientTest.java
@@ -24,7 +24,7 @@ public class ClientTest {
                             socket.getInputStream()));
 
             String request = "GET {\"Request\":\"AddEvent\",\"CalendarName\":\"cal_cock\",\"EventName\":\"eb_cock_super5\",\"EventDescription\":\"I love cocks\",\"EventPicture\":\"no\",\"EventDate\":\"10:30 24/06/2019\",\"EventLocation\":\"cock_city\",\"EventAuthor\":\"pootis@spenser.tf\",\"EventVisibility\":\"true\"}";
-            //String request = "GET {\"Request\":\"DeleteEvent\",\"ID\":\"6\"}";
+            // String request = "GET {\"Request\":\"DeleteEvent\",\"ID\":\"1\"}";
             System.out.println("DONNEE ENVOYEE : " + request);
             pred.println(request);
             //pred.println("{\"Request\":\"DeleteEvent\",\"ID\":\"6\"}");
