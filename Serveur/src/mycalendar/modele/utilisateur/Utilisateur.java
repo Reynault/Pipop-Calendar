f8ccc216c7f184084161d2b82c55b1c7ade5675e
Henkhapi <31958541+Henkhapi@users.noreply.github.com>
2019-03-12 19:09:59 +0100

    Serveur : merge Utilisateur/Notif (with Matthieu)

--- Serveur/src/mycalendar/modele/utilisateur/Utilisateur.java
+++ Serveur/src/mycalendar/modele/utilisateur/Utilisateur.java
@@ -1,11 +1,16 @@
 package mycalendar.modele.utilisateur;
 
 import mycalendar.modele.bdd.GestionnaireBDD;
+import mycalendar.modele.calendrier.Calendrier;
+import mycalendar.modele.calendrier.Evenement;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Classe Utilisateur qui représente un utilisateur dans la base de données
@@ -102,6 +107,7 @@ public class Utilisateur{
         return retour;
     }
 
+
     /**
      * Méthode save qui permet de sauvegarder un utilisateur
      * dans la base de données
@@ -111,4 +117,13 @@ public class Utilisateur{
         int res = 1;
         return res;
     }
+    
+        /**
+     * Getter sur l'email
+     * @return email de l'utilisateur
+     */
+    public String getEmail() {
+        return Email;
+    }
+
 }
