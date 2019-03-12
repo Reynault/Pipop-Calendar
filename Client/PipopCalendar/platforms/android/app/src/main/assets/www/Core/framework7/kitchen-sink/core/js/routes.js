aeaaae944f2480033c4317cb638be5b259523c9a
Cottens <ophe@rocketmail.com>
2019-03-12 15:54:27 +0100

    Client: Modification menus + vue calendrier

--- Client/PipopCalendar/platforms/android/app/src/main/assets/www/Core/framework7/kitchen-sink/core/js/routes.js
+++ Client/PipopCalendar/platforms/android/app/src/main/assets/www/Core/framework7/kitchen-sink/core/js/routes.js
@@ -10,55 +10,15 @@ var mainView = app.views.create('.view-main', {
       componentUrl: './pages/calendar-form.html',
       name: 'calendar-form'
     },
+    {
+      path: '/calendar-view/',
+      componentUrl: './pages/calendar-view.html',
+      name: 'calendar-view'
+    },
     // Default route, match to all pages (e.g. 404 page)
     {
       path: '(.*)',
       url: './pages/404.html'
     }
   ]
-});
-
-var left = app.panel.create({
-  el: '<div class="panel panel-left panel-cover">' +
-    '<div class="page">' +
-      '<div class="navbar no-shadow">' +
-        '<div class="navbar-inner sliding">' +
-          '<div class="title">Navigation</div>' +
-        '</div>' +
-      '</div>' +
-      '<div class="page-content">' +
-        '<div class="list links-list no-hairlines">' +
-          '<ul>' +
-            '<li>' +
-              '<a href="#">Link Item 1</a>' +
-            '</li>' +
-            '<li>' +
-              '<a href="#">Link Item 1</a>' +
-            '</li>' +
-          '</ul>' +
-        '</div>' +
-      '</div>' +
-    '</div>' +
-  '</div>',
-});
-
-var right = app.panel.create({
-  el: '<div class="panel panel-right panel-cover">' +
-    '<div class="page">' +
-      '<div class="navbar no-shadow">' +
-        '<div class="navbar-inner sliding">' +
-          '<div class="title">Settings</div>' +
-        '</div>' +
-      '</div>' +
-      '<div class="page-content">' +
-        '<div class="list links-list no-hairlines">' +
-          '<ul>' +
-            '<li>' +
-              '<a href="/themes/" class="panel-close">Theme</a>' +
-            '</li>' +
-          '</ul>' +
-        '</div>' +
-      '</div>' +
-    '</div>' +
-  '</div>',
 });
