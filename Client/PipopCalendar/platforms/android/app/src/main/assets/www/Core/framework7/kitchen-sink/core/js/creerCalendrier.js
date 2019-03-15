26528c1536f65c5e30b31568d0600cf188a58d94
Alexis <alexis.cesaro8@etu.univ-lorraine.fr>
2019-03-16 00:06:34 +0100

    Client : Ajout de la deconnexion et de scripts pour requêtes vers serveur

---
+++ Client/PipopCalendar/platforms/android/app/src/main/assets/www/Core/framework7/kitchen-sink/core/js/creerCalendrier.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+   chargerCalendrier(localStorage.getItem(""));
+
+  function chargerCalendrier(email){
+      // Il faut crypter les données
+      // Il faut vérifier les données
+      var arr = {"Request":"createCalendar","Email":email, /*-autres informations pour la  création de calendrier*/};
+      console.log("JSON : "+JSON.stringify(arr));
+      app.preloader.show();
+      $.ajax({
+          url: 'http://10.0.2.2:3307',
+          type: 'GET',
+          data: JSON.stringify(arr),
+          dataType: 'text',
+          timeout: 512,
+          async: true,
+          success: function(data, textStatus, jqXHR) {
+              app.preloader.hide();
+              var obj = JSON.parse(data);
+              console.log("data : "+obj["Result"]);
+              if(obj["Result"]==0){
+                window.location = "user-home.html";
+              }else{
+                window.plugins.toast.showWithOptions(
+                {
+                   message: "Erreur : informations non valide",
+                   duration: 1500, // ms
+                   position: "bottom",
+                   addPixelsY: -40,  // (optional) added a negative value to move it up a bit (default 0)
+                   styling: {
+                     opacity: 0.75, // 0.0 (transparent) to 1.0 (opaque). Default 0.8
+                     backgroundColor: '#FF0000', // make sure you use #RRGGBB. Default #333333
+                     textSize: 12, // Default is approx. 13.
+                     cornerRadius: 16, // minimum is 0 (square). iOS default 20, Android default 100
+                     horizontalPadding: 22, // iOS default 16, Android default 50
+                     verticalPadding: 20 // iOS default 12, Android default 30
+                   }
+                  }
+                 );//
+                 $("#connexionErrMsg").append("Connexion error. Please, check your login information.");
+                 $("#emailInput").css("border","2px solid #ff0000");
+                 $("#mdpInput").css("border","2px solid #ff0000");
+                }
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+              console.log("ERREUR : "+jqXHR);
+              console.log("ERREUR : "+textStatus);
+              console.log("ERREUR : "+errorThrown);
+              app.preloader.hide();
+              window.plugins.toast.showWithOptions(
+                  {
+                    message: "Connexion échoué",
+                    duration: 1500, // ms
+                    position: "bottom",
+                    addPixelsY: -40,  // (optional) added a negative value to move it up a bit (default 0)
+                    styling: {
+                          opacity: 0.75, // 0.0 (transparent) to 1.0 (opaque). Default 0.8
+                          backgroundColor: '#FF0000', // make sure you use #RRGGBB. Default #333333
+                          textSize: 12, // Default is approx. 13.
+                          cornerRadius: 16, // minimum is 0 (square). iOS default 20, Android default 100
+                          horizontalPadding: 20, // iOS default 16, Android default 50
+                          verticalPadding: 16 // iOS default 12, Android default 30
+                        }
+                  },
+                  // implement the success callback
+                  function(result) {
+                    if (result && result.event) {
+                      console.log("The toast was tapped or got hidden, see the value of result.event");
+                      console.log("Event: " + result.event); // "touch" when the toast was touched by the user or "hide" when the toast geot hidden
+                      console.log("Message: " + result.message); // will be equal to the message you passed in
+
+                      if (result.event === 'hide') {
+                        console.log("The toast has been shown");
+                      }
+                    }
+                  }
+               );
+          }
+      });
+  }
+});
