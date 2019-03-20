cecb47ab7697448a9ab50d6d807db500e1920763
Jromary <julienromary@yahoo.fr>
2019-03-20 17:03:11 +0100

    Serveur: Delet amis

--- Serveur/src/mycalendar/modele/utilisateur/NotifiCalendrier.java
+++ Serveur/src/mycalendar/modele/utilisateur/NotifiCalendrier.java
@@ -1,10 +1,16 @@
 package mycalendar.modele.utilisateur;
 
 import java.util.Date;
+import java.util.Observable;
 
 public class NotifiCalendrier extends Notif {
 
     public NotifiCalendrier(int id, String email, String type, String message, Date time) {
         super(id, email, type, message, time);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
     }
 }
