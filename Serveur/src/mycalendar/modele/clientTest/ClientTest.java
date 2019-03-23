322f376231fa7aaf5d459e565060777d6bbfac41
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-03-23 01:12:42 +0100

    Serveur : Réglement d'un bug lié aux événements

--- Serveur/src/mycalendar/modele/clientTest/ClientTest.java
+++ Serveur/src/mycalendar/modele/clientTest/ClientTest.java
@@ -53,21 +53,7 @@ public class  ClientTest {
 
     public static void main(String[] args) {
         try{
-            /*lancerClient("{\"Request\":\"AddEvent\"," +
-                    "\"CalendarName\":\"qqq\"," +
-                    "\"EventName\":\"welcome\"," +
-                    "\"EventDescription\":\"red\"," +
-                    "\"EventDate\":\"25/03/2019 20:00\"," +
-                    "\"EventDateFin\":\"25/03/2019 18:00\"," +
-                    "\"EventLocation\":\"Jeudi\"," +
-                    "\"EventVisibility\":\"true\"," +
-                    "\"EventAuthor\":\"test@test.com\"}", true);*/
-            lancerClient("{\"Request\":\"TransfertCalendarOwnership\",\"Calendar\":\"Mario Pissing\",\"OldOwner\":\"test@test\"," +
-                    "\"NewOwner\":\"squeeze@my.hog\"}", true);
-
-           /* DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-            Date dateP = dateFormat.parse("19/03/2019 16:15");
-            System.out.println(dateP.toString());*/
+            lancerClient("{\"Request\":\"LoadEvents\",\"Mail\":\"test@test.com\",\"CalendarName\":\"TOUTENKLAKOS\"}", true);
         } catch (IOException e) {
             e.printStackTrace();
         }
