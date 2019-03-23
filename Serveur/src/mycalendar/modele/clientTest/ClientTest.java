2f9224a9ca8082f59d0aeeefbdbe00d14a2b1477
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-03-23 21:22:24 +0100

    Serveur : Réglement d'un problème lié aux dates

--- Serveur/src/mycalendar/modele/clientTest/ClientTest.java
+++ Serveur/src/mycalendar/modele/clientTest/ClientTest.java
@@ -53,13 +53,10 @@ public class  ClientTest {
 
     public static void main(String[] args) {
         try{
-            lancerClient("{\"Request\":\"ModifyGroup\"," +
+            lancerClient("{\"Request\":\"LoadEvents\"," +
                     "\"Email\":\"test@test.com\","+
                     "\"Mdp\":\"ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff\","+
-                    "\"GroupId\":\"0\"," +
-                    "\"NewName\":\"Nouveaqsdqsdu nom\","+
-                    "\"0\" : \"squeeze@my.hog\""+
-                    "}", true);
+                    "\"CalendarName\":\"TOUTENKLAKOS\"}", true);
         } catch (IOException e) {
             e.printStackTrace();
         }
